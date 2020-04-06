e4c3e8ad9c617d78558c5be701a25e4724da3ba8
Abdelouahed Ennouri <ennouriabdelouahed@gmail.com>
2020-04-06 01:39:53 +0100

    search bar fixed

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -13,6 +13,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<CaseCountry> listCase = new List<CaseCountry>();
+  List<CaseCountry> filtredlistCase = new List<CaseCountry>();
+  final TextEditingController _filter = new TextEditingController();
+  String _searchText = "";
+  Widget _appBarTitle = new Text('Covid-19 track');
 
   @override
   void initState() {
@@ -23,27 +27,83 @@ class _MyHomePageState extends State<MyHomePage> {
               // print(value)
               this.setState(() {
                 listCase = value;
+                filtredlistCase = listCase;
               })
             })
         .catchError((onError) => print(onError));
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Track covid-19"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.0), // here the desired height
+        child: AppBar(
+          centerTitle: true,
+          title: _appBarTitle,
+        ),
       ),
-      body: listCase.length != 0
-          ? ListView.builder(
-              itemCount: listCase.length,
-              itemBuilder: (context, int i) {
-                return Mycard(listCase[i]);
-              },
-            )
-          : Center(
-              child: CircularProgressIndicator(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: new TextField(
+                controller: _filter,
+                onChanged: _ExamplePageState(),
+                decoration: new InputDecoration(
+                    prefixIcon: new Icon(Icons.search),
+                    hintText: 'Search...',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25)))),
+              ),
             ),
+            Expanded(
+              child: listCase.length != 0
+                  ? _buildList()
+                  : Center(child: CircularProgressIndicator()),
+            )
+          ],
+        ),
+      ),
+      resizeToAvoidBottomPadding: false,
+    );
+  }
+
+  _ExamplePageState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filtredlistCase = listCase;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
+
+  Widget _buildList() {
+    if (!(_searchText.isEmpty)) {
+      List<CaseCountry> tempList = new List();
+      for (int i = 0; i < filtredlistCase.length; i++) {
+        if (filtredlistCase[i].country.toLowerCase().contains(_searchText)) {
+          tempList.add(filtredlistCase[i]);
+        }
+      }
+      this.setState(() {
+        filtredlistCase = tempList;
+      });
+    }
+    return ListView.builder(
+      itemCount: listCase == null ? 0 : filtredlistCase.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Mycard(filtredlistCase[index]);
+      },
     );
   }
 }
